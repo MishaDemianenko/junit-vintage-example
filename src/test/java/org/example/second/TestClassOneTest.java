f613cfc42d7342b30c661a16f6fd617982ba9e29
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-11-07 17:53:29 +0100

    Example project with test files in different packages.

---
+++ src/test/java/org/example/second/TestClassOneTest.java
@@ -0,0 +1,14 @@
+package org.example.second;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestClassOneTest
+{
+    @Test
+    public void testSomething()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/org/example/third/TestClassOneTest.java
@@ -0,0 +1,14 @@
+package org.example.third;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestClassOneTest
+{
+    @Test
+    public void testSomething()
+    {
+        assertTrue( true );
+    }
+}
